bede9351d9b74e570012dd86e8ed4eced1d195ea
bymjodfgad <bymjodfgad@gmail.com>
2025-09-29 11:15:21 +0200

    Add project files.

---
+++ Practice0929/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Cache;
+
+namespace Practice0929
+{
+    internal class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            int userAge;
+            bool validLicense;
+
+            Console.WriteLine("Whats your age?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            userAge = Age;
+
+            Console.WriteLine("Do you have a valid drivers license? [Y/N]");
+
+            string License = Console.ReadLine().ToLower();
+
+            if(License == "y")
+            {
+                validLicense = true;
+            }
+            else
+            {
+                validLicense = false;
+            }
+
+            User user = new User(userAge, validLicense);
+            user.IsUserAllowedToDrive();
+
+
+
+
+           
+           
+
+
+        }
+    }
+}
---
+++ Practice0929/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice0929
+{
+    public class User
+    {
+        public int userAge {  get; set; }
+        public bool validLicense { get; set; }
+
+        public User (int userAge, bool validLicense)
+        {
+            this.userAge = userAge;
+            this.validLicense = validLicense;
+        }
+
+        public void IsUserAllowedToDrive()
+        {
+           if (userAge >= 18 && validLicense == true)
+            {
+                Console.WriteLine("User is allowed to drive");
+            }
+           else
+            {
+                Console.WriteLine
+            }
+                
+        }
+    }
+}
